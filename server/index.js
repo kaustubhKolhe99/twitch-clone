6321dc4e0902c789b4a05d5ea86a92e60e76929c
kaustubhKolhe99 <kaustubhkolhe822@outlook.com>
2024-05-30 14:13:19 +0530

    intital server created

---
+++ server/index.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import http from 'http';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import authRouter from './src/routes/authRoutes.js';
+
+dotenv.config();
+
+const PORT = process.env.PORT || process.env.API_PORT;
+
+const app =express();
+
+app.use(express.json());
+
+app.use(cors());
+app.use("/api/auth",authRouter)
+app.get("/", (req, res)=>{
+    return res.send("Hello here is your server")
+})
+const server = http.createServer(app)
+
+server.listen(PORT, ()=>{
+    console.log(`Server started on ${PORT}`);
+
+})
