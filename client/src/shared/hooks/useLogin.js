9ccee1e836344f4fd5452a92f68f1dffd00e53c8
kaustubhKolhe99 <kaustubhkolhe822@outlook.com>
2024-06-05 14:43:13 +0530

    client side finished

--- client/src/shared/hooks/useLogin.js
+++ client/src/shared/hooks/useLogin.js
@@ -1,33 +1,39 @@
-import { useNavigate } from 'react-router-dom';
-import {useState} from 'react'
-import {login as loginRequest} from "../../api"
-import toast from 'react-hot-toast';
-
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { login as loginRequest } from "../../api";
+import toast from "react-hot-toast";
 
 export const useLogin = () => {
-    const {isLoading, setIsLoading} = useState(false); 
-    const navigate = useNavigate()
+  const [isLoading, setIsLoading] = useState(false);
 
-    const login = async (email, password) =>{
-        setIsLoading(false);
-        const response = await loginRequest({
-            email,
-            password,
-        });
+  const navigate = useNavigate();
 
-        setIsLoading(false);
+  const login = async (email, password) => {
+    setIsLoading(true);
 
-        if(response.error){
-            return toast.error(response.exception?.response?.data  || 'Error occurred while logging in please try again')
-        }
-        const {userDetails} = response.data;
+    const response = await loginRequest({
+      email,
+      password,
+    });
 
-        localStorage.setItem("user", userDetails)
-        navigate('/')
+    setIsLoading(false);
 
-        return {
-            login,
-            isLoading,
-        }
+    if (response.error) {
+      return toast.error(
+        response.exception?.response?.data ||
+          "Error occured while loggin in. Please try again"
+      );
     }
-}
+
+    const { userDetails } = response.data;
+
+    localStorage.setItem("user", JSON.stringify(userDetails));
+
+    navigate("/");
+  };
+
+  return {
+    login,
+    isLoading,
+  };
+};
