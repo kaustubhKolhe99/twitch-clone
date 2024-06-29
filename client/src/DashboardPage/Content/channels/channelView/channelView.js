c14b58fdd383b3cf99e3dfb69a20c0699e7dc59b
kaustubhKolhe99 <kaustubhkolhe822@outlook.com>
2024-06-29 20:28:32 +0530

    real time media server created

--- client/src/DashboardPage/Content/channels/channelView/channelView.js
+++ client/src/DashboardPage/Content/channels/channelView/channelView.js
@@ -4,7 +4,17 @@ import { ChannelDescription } from "./channelDescription";
 import { useChannelDetails } from "../../../../shared/hooks";
 import { useParams } from "react-router-dom";
 import { LoadingSpinner } from "../../../../shared/components";
+import { ReactFlvPlayer } from 'react-flv-player'
 
+export const Stream = () =>{
+    return <div className="channel-video-container">
+        <ReactFlvPlayer
+            width="100%"
+            height="100%"
+            url="http://localhost:8000/live/7f1b0fc6-85cf-47e3-bcda-4ed456759f63.flv"/>
+
+    </div>
+}
 export const ChannelView = ({getChannels}) =>{
     const { isFetching, getChannelDetails, channelDetails  } = useChannelDetails();
 
@@ -21,9 +31,10 @@ export const ChannelView = ({getChannels}) =>{
     return(
         <div className="channel-container">
             <div className="channel-video-description-section">
-                <div className="channel-offline-placeholder">
+                {/* <div className="channel-offline-placeholder">
                     <span>Channel is offline</span>
-                </div>
+                </div> */}
+                <Stream/>
 
                 <ChannelDescription
                     channelId= {channelDetails.id}
